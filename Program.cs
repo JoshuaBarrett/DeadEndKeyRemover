ae244bc90e996a2246768af8445699dddd74bb0b
JoshuaBarrett <joshua.barrett@gmail.com>
2023-05-25 03:11:58 +0100

    First draft complete

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using DeadEndKeyRemover;
+using Microsoft.Win32;
+using System.Runtime.InteropServices;
+
+string default_uninstallKeyName = "SOFTWARE\\WOW6432Node\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+
+Console.WriteLine("Welcome to Dead-End Key Remover App\n");
+
+if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+{
+    Console.WriteLine("Detected non-windows OS");
+} else
+{
+    Console.WriteLine($"Enter the Registry path to the Uninstall Keys or press enter for Default: \n({default_uninstallKeyName})");
+    string uninstallKeyName = Console.ReadLine() ?? default_uninstallKeyName;
+
+    Console.WriteLine($"Enter filter for display name, this is the name of the app as it appears under Apps & Features: ");
+    string displayNameFilter = Console.ReadLine() ?? string.Empty;
+
+    RegistryClient registryClient = new RegistryClient(uninstallKeyName);
+    string[] deadEndKeys = registryClient.GetDeadEndKeys(displayNameFilter);
+
+    Console.WriteLine("The following Display Names have been found to be Dead End keys within the Registry:-");
+    foreach(string k in deadEndKeys)
+    {
+        Console.WriteLine(k);
+    }
+    Console.WriteLine("Confirm removal of these keys (y / n): ");
+    string removalResponse = Console.ReadLine() ?? string.Empty;
+    if (removalResponse.Equals("y", StringComparison.CurrentCultureIgnoreCase))
+    {
+        registryClient.RemoveSubKeys(deadEndKeys);
+        Console.WriteLine("Dead End keys removed");
+    } else
+    {
+        Console.WriteLine("Removal canceled");
+    }
+}
+
+Console.Write("\nPress any key to exit");
+Console.ReadKey();
---
+++ RegistryClient.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadEndKeyRemover
+{
+    [SupportedOSPlatform("windows")]
+    public class RegistryClient
+    {
+        RegistryKey? MainRegistryKey;
+
+        public RegistryClient(string mainKeyPath)
+        {
+            this.MainRegistryKey = Registry.LocalMachine.OpenSubKey(mainKeyPath, true);
+        }
+
+        public string[] GetDeadEndKeys(string displayNameFilter)
+        {
+            if (this.MainRegistryKey == null)
+            {
+                throw new NullReferenceException("Main Registry is Null");
+            }
+
+            string[] subKeys = this.MainRegistryKey.GetSubKeyNames();
+
+            string[] filteredSubKeys = subKeys.Where(x => x.Contains(displayNameFilter, StringComparison.CurrentCultureIgnoreCase)).ToArray();
+
+            string[] deadEndSubKeys = FindDeadEndSubKeys(filteredSubKeys).ToArray();
+
+            return deadEndSubKeys;
+        }
+
+        public void RemoveSubKeys(string[] subKeyNames)
+        {
+            if (this.MainRegistryKey == null)
+            {
+                throw new NullReferenceException("Main Registry is Null");
+            }
+
+            foreach (string subKey in subKeyNames)
+            {
+                this.MainRegistryKey.DeleteSubKey(subKey);
+            }
+        }
+
+        private IEnumerable<string> FindDeadEndSubKeys(string[] subKeyNames)
+        {
+            if (this.MainRegistryKey == null)
+            {
+                throw new NullReferenceException("Main Registry is Null");
+            }
+
+            foreach (string keyName in subKeyNames)
+            {
+                RegistryKey? subRegKey = this.MainRegistryKey.OpenSubKey(keyName, true);
+                if (subRegKey == null)
+                {
+                    throw new NullReferenceException("Sub Registry is Null");
+                }
+
+                string uninstallFilePath = (string)(subRegKey.GetValue("UninstallString") ?? string.Empty);
+                if (string.IsNullOrEmpty(uninstallFilePath) || !File.Exists(uninstallFilePath))
+                {
+                    yield return keyName;
+                }                
+            }            
+        }
+    }
+}
